b9eb85ef15a7c881e514516af3c30dfa7072d091
Patryk Milkiewicz <g00397265@atu.ie>
2022-11-16 14:24:02 +0000

    Added a new class to get the passenger by ID

--- src/main/java/ie/atu/Passenger2/Passenger2Application.java
+++ src/main/java/ie/atu/Passenger2/Passenger2Application.java
@@ -3,6 +3,7 @@ package ie.atu.Passenger2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +27,11 @@ public class Passenger2Application {
 				new Passenger("Mr", "Patryk3", "548735647", "0859482163", 50),
 				new Passenger("Mr", "Patryk4", "534897328", "0858685763", 19));
 				return myPassengers;
+	}
+
+	@GetMapping("/{passengerID}")
+	public Passenger getPassenger(@PathVariable String passengerID){
+		Passenger myPassenger = new Passenger("Mr", "Patryk5", "123784456789", "0859485767", 25);
+		return myPassenger;
 	}
 }
