79337dccec6a28011a6b1cc4e09bbde657439947
Patryk Milkiewicz <g00397265@atu.ie>
2022-11-16 13:33:20 +0000

    Added Spring and connected to local Host, created some new passengers and displayed them on a local server

--- src/main/java/ie/atu/Passenger2/Passenger2Application.java
+++ src/main/java/ie/atu/Passenger2/Passenger2Application.java
@@ -21,7 +21,7 @@ public class Passenger2Application {
 	@GetMapping
 	public List<Passenger> getPassenger(){
 		List<Passenger> myPassengers = List.of(
-				new Passenger("Mr", "Patryk", "123456789", "0859485767", 25),
+				new Passenger("Mr", "Patryk1", "123456789", "0859485767", 25),
 				new Passenger("Mr", "Patryk2", "987654321", "0859475763", 34),
 				new Passenger("Mr", "Patryk3", "548735647", "0859482163", 50),
 				new Passenger("Mr", "Patryk4", "534897328", "0858685763", 19));
