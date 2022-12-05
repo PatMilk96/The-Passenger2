417fa35948d33ab3296324ce516f66386084ebfb
Patryk Milkiewicz <g00397265@atu.ie>
2022-12-05 21:19:42 +0000

    updated test case

--- src/main/java/ie/atu/Passenger2/Passenger2Application.java
+++ src/main/java/ie/atu/Passenger2/Passenger2Application.java
@@ -2,30 +2,11 @@ package ie.atu.Passenger2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-@RestController
 @SpringBootApplication
-@RequestMapping(path="api/passenger")
 public class Passenger2Application {
-
-	PassengerService myService;
-
-	public Passenger2Application(PassengerService MyService){
-		this.myService = myService;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Passenger2Application.class, args);
-	}
-
-	@GetMapping("/{passengerID}")
-	public Passenger getPassenger(@PathVariable String passengerID){
-		return myService.getPassenger(passengerID);
 	}
 }
